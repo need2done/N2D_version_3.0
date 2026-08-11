6c94ff3b48f91e355919f2858020e56b8ed830d0
GramioGO <gramiogoo@gmail.com>
2026-08-11 22:22:04 +0530

    Fix V&F product card price rendering logic

---
+++ website/vegetables-fruits/_next/static/9Ck80HgEQMECtW3vBychB/_clientMiddlewareManifest.js
@@ -0,0 +1,6 @@
+self.__MIDDLEWARE_MATCHERS = [
+  {
+    "regexp": "^\\/vegetables-fruits(?:\\/(_next\\/data\\/[^/]{1,}))?\\/admin(?:\\/((?:[^\\/#\\?]+?)(?:\\/(?:[^\\/#\\?]+?))*))?(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
+    "originalSource": "/admin/:path*"
+  }
+];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
