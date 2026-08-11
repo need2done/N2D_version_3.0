ad8b10e0c1978433802b20791e8e640cc7c8b2d4
GramioGO <gramiogoo@gmail.com>
2026-08-11 22:37:20 +0530

    Clean rebuild V&F to fix old cached test numbers

--- website/vegetables-fruits/_next/static/tqlZnuEs8tAeB22jVSFWP/_clientMiddlewareManifest.js
+++
@@ -1,6 +0,0 @@
-self.__MIDDLEWARE_MATCHERS = [
-  {
-    "regexp": "^\\/vegetables-fruits(?:\\/(_next\\/data\\/[^/]{1,}))?\\/admin(?:\\/((?:[^\\/#\\?]+?)(?:\\/(?:[^\\/#\\?]+?))*))?(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
-    "originalSource": "/admin/:path*"
-  }
-];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
